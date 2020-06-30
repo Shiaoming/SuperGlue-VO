97f0f9463c8dcb6f5f171769d57eee00a162a2b8
Shiaoming <xmzhao@buaa.edu.cn>
2020-06-30 21:19:30 +0800

    add a simple superpoint+superglue

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 
 from norm_visual_odometry import PinholeCamera, VisualOdometry
 from sp_visual_odometry import VisualOdometry as sp_VisualOdometry
+from sp_visual_odometry import VisualOdometry as spglue_VisualOdometry
 
 # kitti sequence
 Kitti_path = "/mnt/data/datasets/public/KITTI/KITTI/odometry"
@@ -23,6 +24,7 @@ else:
 pose_path = Kitti_path + '/poses/' + Num + '.txt'
 vo = VisualOdometry(cam, pose_path)
 sp_vo = sp_VisualOdometry(cam, pose_path)
+spg_vo = sp_VisualOdometry(cam, pose_path)
 
 traj = np.zeros((600, 600, 3), dtype=np.uint8)
 
@@ -31,13 +33,23 @@ log_fopen = open("results/kitti_" + Num + ".txt", mode='a')
 
 # list
 sp_errors = []
+spg_errors = []
 norm_errors = []
+spg_feature_nums = []
 sp_feature_nums = []
 norm_feature_nums = []
 
 for img_id in range(4541):
     img = cv2.imread(Kitti_path + '/sequences/' +
                      Num + '/image_0/' + str(img_id).zfill(6) + '.png', 0)
+
+    # superpoint + superglue
+    spg_vo.update(img, img_id)
+    spg_cur_t = spg_vo.cur_t
+    if (img_id > 2):
+        spg_x, spg_y, spg_z = spg_cur_t[0], spg_cur_t[1], spg_cur_t[2]
+    else:
+        spg_x, spg_y, spg_z = 0., 0., 0.
 
     # === superpoint ==============================
     sp_vo.update(img, img_id)
@@ -68,21 +80,27 @@ for img_id in range(4541):
     # === calculation =============================
     # calculate error
     sp_est_point = np.array([sp_x, sp_z]).reshape(2)
+    spg_est_point = np.array([spg_x, spg_z]).reshape(2)
     norm_est_point = np.array([x, z]).reshape(2)
     gt_point = np.array([sp_vo.trueX, sp_vo.trueZ]).reshape(2)
     sp_error = np.linalg.norm(sp_est_point - gt_point)
+    spg_error = np.linalg.norm(spg_est_point - gt_point)
     norm_error = np.linalg.norm(norm_est_point - gt_point)
 
     # append
     sp_errors.append(sp_error)
+    spg_errors.append(spg_error)
     norm_errors.append(norm_error)
     sp_feature_nums.append(len(sp_vo.px_ref))
+    spg_feature_nums.append(len(spg_vo.px_ref))
     norm_feature_nums.append(len(vo.px_ref))
 
     # average
     avg_sp_error = np.mean(np.array(sp_errors))
+    avg_spg_error = np.mean(np.array(spg_errors))
     avg_norm_error = np.mean(np.array(norm_errors))
     avg_sp_feature_num = np.mean(np.array(sp_feature_nums))
+    avg_spg_feature_num = np.mean(np.array(spg_feature_nums))
     avg_norm_feature_num = np.mean(np.array(norm_feature_nums))
 
     # === log writer ==============================
@@ -93,22 +111,28 @@ for img_id in range(4541):
     # === drawer ==================================
     # each point
     sp_draw_x, sp_draw_y = int(sp_x) + 290, int(sp_z) + 90
+    spg_draw_x, spg_draw_y = int(spg_x) + 290, int(spg_z) + 90
     norm_draw_x, norm_draw_y = int(x) + 290, int(z) + 90
     true_x, true_y = int(sp_vo.trueX) + 290, int(sp_vo.trueZ) + 90
 
     # draw trajectory
     cv2.circle(traj, (sp_draw_x, sp_draw_y), 1, (255, 0, 0), 1)
+    cv2.circle(traj, (spg_draw_x, spg_draw_y), 1, (255, 255, 255), 1)
     cv2.circle(traj, (norm_draw_x, norm_draw_y), 1, (0, 255, 0), 1)
     cv2.circle(traj, (true_x, true_y), 1, (0, 0, 255), 2)
-    cv2.rectangle(traj, (10, 20), (600, 60), (0, 0, 0), -1)
+    cv2.rectangle(traj, (10, 20), (600, 80), (0, 0, 0), -1)
     # draw text
     text = "Superpoint: [AvgFeature] %4.2f [AvgError] %2.4fm" % (
         avg_sp_feature_num, avg_sp_error)
     cv2.putText(traj, text, (20, 40),
                 cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1, 8)
+    text = "Superpoint + SuperGlue: [AvgFeature] %4.2f [AvgError] %2.4fm" % (
+        avg_spg_feature_num, avg_spg_error)
+    cv2.putText(traj, text, (20, 60),
+                cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, 8)
     text = "Normal    : [AvgFeature] %4.2f [AvgError] %2.4fm" % (
         avg_norm_feature_num, avg_norm_error)
-    cv2.putText(traj, text, (20, 60),
+    cv2.putText(traj, text, (20, 80),
                 cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1, 8)
 
     cv2.imshow('Road facing camera', np.concatenate((sp_img, img), axis=0))
---
+++ spglue_visual_odometry.py
@@ -0,0 +1,124 @@
+import numpy as np
+import torch
+import cv2
+
+from models.matching import Matching
+
+STAGE_FIRST_FRAME = 0
+STAGE_SECOND_FRAME = 1
+STAGE_DEFAULT_FRAME = 2
+
+
+class PinholeCamera:
+    def __init__(self, width, height, fx, fy, cx, cy,
+                 k1=0.0, k2=0.0, p1=0.0, p2=0.0, k3=0.0):
+        self.width = width
+        self.height = height
+        self.fx = fx
+        self.fy = fy
+        self.cx = cx
+        self.cy = cy
+        self.distortion = (abs(k1) > 0.0000001)
+        self.d = [k1, k2, p1, p2, k3]
+
+
+class VisualOdometry:
+    def __init__(self, cam, annotations):
+        self.frame_stage = 0
+        self.cam = cam
+        self.new_frame = None
+        self.last_frame = None
+        self.cur_R = None
+        self.cur_t = None
+        self.px_ref = None
+        self.px_cur = None
+        self.focal = cam.fx
+        self.pp = (cam.cx, cam.cy)
+        self.trueX, self.trueY, self.trueZ = 0, 0, 0
+
+        nms_dist = 4
+        conf_thresh = 0.015
+        sinkhorn_iterations = 20
+        match_threshold = 0.2
+        cuda = False
+        config = {
+            'superpoint': {
+                'nms_radius': nms_dist,
+                'keypoint_threshold': conf_thresh,
+                'max_keypoints': -1
+            },
+            'superglue': {
+                'weights': 'indoor',
+                'sinkhorn_iterations': sinkhorn_iterations,
+                'match_threshold': match_threshold,
+            }
+        }
+
+        device = 'cuda' if torch.cuda.is_available() and cuda else 'cpu'
+
+        self.keys = ['keypoints', 'scores', 'descriptors']
+        self.matching = Matching(config).eval().to(device)
+
+        with open(annotations) as f:
+            self.annotations = f.readlines()
+
+    def featureTracking(self):
+        pred = self.matching({**self.last_data, 'image1': self.new_frame})
+        kp1 = self.last_data['keypoints0'][0].cpu().numpy()
+        kp2 = pred['keypoints1'][0].cpu().numpy()
+        return kp1, kp2
+
+    def getAbsoluteScale(self, frame_id):  # specialized for KITTI odometry dataset
+        ss = self.annotations[frame_id - 1].strip().split()
+        x_prev = float(ss[3])
+        y_prev = float(ss[7])
+        z_prev = float(ss[11])
+        ss = self.annotations[frame_id].strip().split()
+        x = float(ss[3])
+        y = float(ss[7])
+        z = float(ss[11])
+        self.trueX, self.trueY, self.trueZ = x, y, z
+        return np.sqrt((x - x_prev) * (x - x_prev) + (y - y_prev) * (y - y_prev) + (z - z_prev) * (z - z_prev))
+
+    def processFirstFrame(self):
+        self.last_data = self.matching.superpoint({'image': self.new_frame})
+        self.last_data = {k + '0': self.last_data[k] for k in self.keys}
+        self.last_data['image0'] = self.new_frame
+        self.frame_stage = STAGE_SECOND_FRAME
+
+    def processSecondFrame(self):
+        self.px_ref, self.px_cur = self.featureTracking()
+
+        E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref,
+                                       focal=self.focal, pp=self.pp,
+                                       method=cv2.RANSAC, prob=0.999, threshold=1.0)
+        _, self.cur_R, self.cur_t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref,
+                                                          focal=self.focal, pp=self.pp)
+        self.frame_stage = STAGE_DEFAULT_FRAME
+        self.px_ref = self.px_cur
+
+    def processFrame(self, frame_id):
+        self.px_ref, self.px_cur = self.featureTracking()
+
+        E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref,
+                                       focal=self.focal, pp=self.pp,
+                                       method=cv2.RANSAC, prob=0.999, threshold=1.0)
+        _, R, t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref,
+                                        focal=self.focal, pp=self.pp)
+        absolute_scale = self.getAbsoluteScale(frame_id)
+        if (absolute_scale > 0.1):
+            self.cur_t = self.cur_t + absolute_scale * self.cur_R.dot(t)
+            self.cur_R = R.dot(self.cur_R)
+        self.px_ref = self.px_cur
+
+    def update(self, img, frame_id):
+        assert (img.ndim == 2 and img.shape[0] == self.cam.height and img.shape[1] ==
+                self.cam.width), "Frame: provided image has not the same size as the camera model or image is not grayscale"
+        self.new_frame = img
+        if (self.frame_stage == STAGE_DEFAULT_FRAME):
+            self.processFrame(frame_id)
+        elif (self.frame_stage == STAGE_SECOND_FRAME):
+            self.processSecondFrame()
+        elif (self.frame_stage == STAGE_FIRST_FRAME):
+            self.processFirstFrame()
+        self.last_frame = self.new_frame
